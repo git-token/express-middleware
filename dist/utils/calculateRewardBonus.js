e7fc4dbd694e26aa5043ef9b8ba549cb3df6d653
Ryan Tate <ryan.michael.tate@gmail.com>
2017-06-21 14:30:51 -0700

    Console logging request body attributes for calculating reward

--- dist/utils/calculateRewardBonus.js
+++ dist/utils/calculateRewardBonus.js
@@ -12,9 +12,17 @@ var _bluebird2 = _interopRequireDefault(_bluebird);
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 function calculateRewardBonus(_ref) {
-  var repository = _ref.repository;
+  var repository = _ref.repository,
+      commits = _ref.commits,
+      head_commit = _ref.head_commit;
 
   return new _bluebird2.default(function (resolve, reject) {
-    resolve(1000000);
+    var size = repository.size;
+
+    console.log('calculateRewardBonus::repository', repository);
+    console.log('calculateRewardBonus::commits', commits);
+    console.log('calculateRewardBonus::head_commit', head_commit);
+
+    resolve(1e18 / size);
   });
 }
--- src/utils/calculateRewardBonus.js
+++ src/utils/calculateRewardBonus.js
@@ -1,7 +1,12 @@
 import Promise, { join, promisifyAll } from 'bluebird'
 
-export default function calculateRewardBonus({ repository }) {
+export default function calculateRewardBonus({ repository, commits, head_commit }) {
   return new Promise((resolve, reject) => {
-    resolve(1000000)
+    const { size } = repository
+    console.log('calculateRewardBonus::repository', repository)
+    console.log('calculateRewardBonus::commits', commits)
+    console.log('calculateRewardBonus::head_commit', head_commit)
+
+    resolve(1e18/size)
   })
 }
