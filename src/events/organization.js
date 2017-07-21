ab85c615636788a4ccabf26e34c5b72340fd2494
Ryan Tate <ryan.michael.tate@gmail.com>
2017-07-21 13:36:12 -0700

    This msg is automated

--- src/events/organization.js
+++ src/events/organization.js
@@ -22,12 +22,14 @@ export default function organization ({ event, data }) {
         }).then(() => {
           return this.generateReward({
             rewardType: event,
-            deliveryID: headers['x-github-delivery'],
+            // Note that if deriving mutiple events from a single event, the delivery ID must be unique,
+            // Suggest appending a value to the deliveryID, e.g. "ID"+"B"
+            deliveryID: `${headers['x-github-delivery']}B`,
             // contributorUsername in this case should be the contract address;
             // basically the contract should hold the rewards until the milestone is // reached. Tokens will be auctioned on behalf of the project for funding.
             contributorUsername: body['membership']['user']['login'],
             rewardBonus: 0,
-            reservedValue: 15000 * Math.pow(10, decimals),
+            reservedValue: Number(15000 * Math.pow(10, decimals)),
           })
         }).then((result) => {
           resolve(result)
