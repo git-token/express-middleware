77116bec7f262813740cd7569fa8469a50cf8997
Ryan Tate <ryan.michael.tate@gmail.com>
2017-08-27 19:08:33 -0700

    Updating faucet endpoint; TODO, breakout faucet into independent service

--- src/utils/faucet.js
+++ src/utils/faucet.js
@@ -5,7 +5,7 @@ import rp from 'request-promise'
 export default function faucet() {
   return new Promise((resolve, reject) => {
     rp({
-      uri: `https://gittoken.org/gittoken/faucet/0x${this.ks.getAddresses()[0]}`,
+      uri: `https://gittoken.io/gittoken/faucet/0x${this.ks.getAddresses()[0]}`,
       method: 'POST',
     }).then((response) => {
       // console.log('faucet::response', response)
